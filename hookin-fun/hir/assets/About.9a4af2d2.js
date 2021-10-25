9746291430a0f1f23e83a3575c55e62360830d09
Hookin <45350455+LuckyHookin@users.noreply.github.com>
2021-10-26 00:11:16 +0800

    update

---
+++ hookin-fun/hir/assets/About.9a4af2d2.js
@@ -0,0 +1,1 @@
+import{b as s,N as n}from"./index.79f2e216.js";import{o,d as r,h as c,k as e,v as i,x as u,b as d,F as l}from"./vendor.257b0695.js";const m={class:"container relative mx-auto min-h-screen pt-16"},N={setup(p){const t=s();function a(){t.id++}return(f,_)=>(o(),r(l,null,[c(n),e("div",m,[e("div",null,[i(u(d(t).id)+" ",1),e("button",{onClick:a},"++")])])],64))}};export{N as default};
